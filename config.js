1086cc8a36909010b81649800aa11e78f4006f80
Contantine Kim <elegantcoder@gmail.com>
2014-07-16 02:21:08 +0900

    initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  server: {
+    base: '/'
+    , host: 'localhost'
+    , target: 'http://www.google-analytics.com'
+    , port: 9010
+    , bindAddress: '0.0.0.0'
+  },
+  plugins: []
+};
