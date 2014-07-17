2b26a660874975964f4b063f45240c134e50ce57
Contantine Kim <elegantcoder@gmail.com>
2014-07-17 23:51:24 +0900

    Fix for https://ssl.ggogle-analytics.com

--- config.local.js
+++ config.local.js
@@ -2,6 +2,7 @@ module.exports = {
   server: {
     base: '/'
     , host: 'localhost'
+    , secureHost: 'https://localhost'
     , target: 'http://www.google-analytics.com'
     , port: 9010
     , bindAddress: '0.0.0.0'
